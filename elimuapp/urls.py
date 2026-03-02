12af292aa442e3e6956e9b357749d0940c03243d
samuelmwangi07 <wamarunda2007@gmail.com>
2026-03-02 09:08:06 +0300

    new commit

--- elimuapp/urls.py
+++ elimuapp/urls.py
@@ -5,5 +5,13 @@ from elimuapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('coursedetails/', views.coursedeatils, name='coursedetails'),
+    path('courses/', views.courses, name='courses'),
+    path('events/', views.events, name='events'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('starter/', views.starter, name='starter'),
+    path('trainers/', views.home, name='home'),
 ]
--- elimuapp/views.py
+++ elimuapp/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def coursedetails(request):
+    return render(request, 'course-details.html')
+def courses(request):
+    return render(request, 'courses.html')
+def events(request):
+    return render(request, 'events.html')
+def pricing(request):
+    return render(request, 'pricing.html')
+def starter(request):
+    return render(request, 'starter-page.html')
+def trainers(request):
+    return render(request, 'trainers.html')
